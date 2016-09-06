bd17090135e953c9733f5b2d7249174fa2ca80ba
Nathan Scherneck <nathan.scherneck@gmail.com>
2016-09-06 11:12:16 -0700

    first commit?

--- public/admin/logfile.php
+++ public/admin/logfile.php
@@ -6,6 +6,7 @@ if(!$session->is_logged_in()) {redirect_to("login.php");}
 ?>
 
 <?php include_layout_template('admin_header.php'); ?>
+
       <a href="index.php">Home</a><br><br>
       <h2>Event Log</h2>
       <hr>
