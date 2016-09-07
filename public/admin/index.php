2fd08fc37ea29604f8cc375677e6bd3c75dd25b2
Nathan Scherneck <nathan.scherneck@gmail.com>
2016-09-07 13:02:56 -0700

    added a logout page

--- public/admin/index.php
+++ public/admin/index.php
@@ -9,5 +9,6 @@ if(!$session->is_logged_in()) {redirect_to("login.php");}
 
       <h2>Menu</h2>
       <a href="logfile.php">Event Log</a><br>
+      <a href="logout.php">Logout</a><br>
 
 <?php include_layout_template('admin_footer.php'); ?>
---
+++ public/admin/logout.php
@@ -0,0 +1,8 @@
+<?php //logout.php
+require_once ("../../includes/initialize.php");
+
+    log_action("Logout: ", $_SESSION['user_id'] . " logged out.");
+    $session->logout();
+    redirect_to("login.php");
+
+?>
